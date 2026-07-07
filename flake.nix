a4292753499c13aaf6c73a68e9fc832aff8a542c
Sreehari Anil <sreehari7102008@gmail.com>
2026-07-07 13:22:02 +0530

    init: nix-config

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "Sreehari's Home Manager configuration";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    { nixpkgs, home-manager, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in
+    {
+      homeConfigurations.sreehari = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+
+        modules = [
+          ./home.nix
+        ];
+      };
+    };
+}
---
+++ home.nix
@@ -0,0 +1,16 @@
+{ config, pkgs, ... }:
+
+{
+  home.username = "sreehari";
+  home.homeDirectory = "/home/sreehari";
+
+  home.stateVersion = "26.05";
+
+  programs.home-manager.enable = true;
+
+  home.packages = with pkgs; [
+    helix
+    fastfetch
+    git
+  ];
+}
