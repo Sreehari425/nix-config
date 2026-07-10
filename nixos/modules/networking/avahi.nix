df4b3e3209792e443f9568a8c0ac46d49700fa5d
Sreehari Anil <sreehari7102008@gmail.com>
2026-07-10 16:37:16 +0530

    dev: added firewall rules ,tailscale , and more

--- nixos/modules/networking/avahi.nix
+++ nixos/modules/networking/avahi.nix
@@ -1,5 +1,19 @@
 { ... }:
-
 {
-
+  services.avahi = {
+    enable = true;
+    ipv4 = true;
+    ipv6 = true;
+    nssmdns4 = true; # mDNS resolution for .local domains
+    publish = {
+      enable = true;
+      hinfo = false;
+      workstation = false;
+    };
+    extraConfig = ''
+      [server]
+      ratelimit-interval-usec=1000000
+      ratelimit-burst=1000
+    '';
+  };
 }
--- nixos/modules/networking/firewall.nix
+++ nixos/modules/networking/firewall.nix
@@ -1,5 +1,26 @@
-{ ... }:
-
+{ pkgs, ... }:
 {
+  networking.nftables.enable = true;
 
+  services.firewalld = {
+    enable = true;
+    package = pkgs.firewalld;
+  };
+
+  # Public zone - mirrors your Arch setup
+  environment.etc."firewalld/zones/public.xml".text = ''
+    <?xml version="1.0" encoding="utf-8"?>
+    <zone>
+      <short>Public</short>
+      <description>For use in public areas.</description>
+      <service name="dhcpv6-client"/>
+      <service name="kdeconnect"/>
+      <service name="mdns"/>
+      <port protocol="tcp" port="53317"/>
+      <port protocol="udp" port="53317"/>
+      <port protocol="udp" port="7884"/>
+      <port protocol="tcp" port="7889"/>
+      <forward/>
+    </zone>
+  '';
 }
