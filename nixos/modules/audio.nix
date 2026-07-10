5de05f6d6e9e0f65ad6de5396d31d9d6094ed7ea
Sreehari Anil <sreehari7102008@gmail.com>
2026-07-10 22:07:17 +0530

    dev: add audio support

--- nixos/modules/audio.nix
+++ nixos/modules/audio.nix
@@ -1,4 +1,16 @@
-{ ... }:
-
 {
+  services.pipewire = {
+    enable = true;
+
+    alsa.enable = true;
+    alsa.support32Bit = true;
+
+    pulse.enable = true;
+
+    jack.enable = true;
+
+    wireplumber.enable = true;
+  };
+
+  security.rtkit.enable = true;
 }
