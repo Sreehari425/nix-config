118f5d23371b0c03202b4ae5926415ac14417db3
Sreehari Anil <sreehari7102008@gmail.com>
2026-07-10 16:41:00 +0530

    dev: converted firewall rules to nix exepr

--- nixos/modules/networking/firewall.nix
+++ nixos/modules/networking/firewall.nix
@@ -5,22 +5,37 @@
   services.firewalld = {
     enable = true;
     package = pkgs.firewalld;
-  };
 
-  # Public zone - mirrors your Arch setup
-  environment.etc."firewalld/zones/public.xml".text = ''
-    <?xml version="1.0" encoding="utf-8"?>
-    <zone>
-      <short>Public</short>
-      <description>For use in public areas.</description>
-      <service name="dhcpv6-client"/>
-      <service name="kdeconnect"/>
-      <service name="mdns"/>
-      <port protocol="tcp" port="53317"/>
-      <port protocol="udp" port="53317"/>
-      <port protocol="udp" port="7884"/>
-      <port protocol="tcp" port="7889"/>
-      <forward/>
-    </zone>
-  '';
+    zones.public = {
+      short = "Public";
+      description = "For use in public areas.";
+
+      services = [
+        "dhcpv6-client"
+        "kdeconnect"
+        "mdns"
+      ];
+
+      ports = [
+        {
+          port = 53317;
+          protocol = "tcp";
+        }
+        {
+          port = 53317;
+          protocol = "udp";
+        }
+        {
+          port = 7884;
+          protocol = "udp";
+        }
+        {
+          port = 7889;
+          protocol = "tcp";
+        }
+      ];
+
+      forward = true;
+    };
+  };
 }
