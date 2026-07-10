f439a21fa8fb3481647f7cee6b33053d5cee739c
Sreehari Anil <sreehari7102008@gmail.com>
2026-07-10 20:29:41 +0530

    dev: add users

--- nixos/modules/users.nix
+++ nixos/modules/users.nix
@@ -1,4 +1,24 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
+  programs.bash.enable = true;
+
+  users.users.sreehari = {
+    isNormalUser = true;
+    description = "Sreehari";
+
+    extraGroups = [
+      "wheel"
+      "video"
+      "audio"
+      "input"
+      "uucp"
+      "scanner"
+      "lp"
+
+      # "docker"       # <-- Keep these commented out until you enable
+      # "libvirt"      # <-- virtualization/docker services later, or NixOS will crash!
+    ];
+    shell = pkgs.bash;
+  };
 }
