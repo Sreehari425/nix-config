adf7de073ab1e8481d87cb69ce71abae430f2504
Sreehari Anil <sreehari7102008@gmail.com>
2026-07-10 14:59:51 +0530

    dev: added place holders for audio boot desktop netowrking and users

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -1,4 +1,13 @@
-{ config, pkgs, ... }:
+{ ... }:
 
 {
+  imports = [
+    # ./hardware-configuration.nix
+
+    ./modules/boot.nix
+    ./modules/networking.nix
+    ./modules/users.nix
+    ./modules/audio.nix
+    ./modules/desktop.nix
+  ];
 }
