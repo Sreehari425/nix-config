619d5c12fbdd53f6e04f6d8059909462d0efe133
Sreehari Anil <sreehari7102008@gmail.com>
2026-07-13 16:58:46 +0530

    dev: fix mozc bruvh

--- home/modules/ime.nix
+++ home/modules/ime.nix
@@ -1,20 +1,22 @@
 { pkgs, ... }:
 
 {
-  home.packages = with pkgs; [
-    fcitx5
-    fcitx5-mozc
-    fcitx5-gtk
-    qt6Packages.fcitx5-configtool
-  ];
+  home.packages = [ ];
+
+  i18n.inputMethod = {
+    enable = true;
+    type = "fcitx5";
+    fcitx5.addons = with pkgs; [
+      fcitx5-mozc
+      fcitx5-gtk
+    ];
+  };
 
   home.sessionVariables = {
-    GTK_IM_MODULE = "fcitx";
-    QT_IM_MODULE = "fcitx";
     XMODIFIERS = "@im=fcitx";
     INPUT_METHOD = "fcitx";
     SDL_IM_MODULE = "fcitx";
   };
-  xdg.configFile."fcitx5".source = ../configs/fcitx5;
 
+  xdg.configFile."fcitx5".source = ../configs/fcitx5;
 }
