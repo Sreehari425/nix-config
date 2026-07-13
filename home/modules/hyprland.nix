348e25eec4cd529547fc2d10f306736164d1e8ec
Sreehari Anil <sreehari7102008@gmail.com>
2026-07-13 23:38:02 +0530

    dev: add swaync

--- home/modules/hyprland.nix
+++ home/modules/hyprland.nix
@@ -1,7 +1,15 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   services.hypridle.enable = true;
 
   # programs.hyprlock.enable = true;
+  # Inside your home.nix
+  home.packages = with pkgs; [
+    libnotify # Provides the 'notify-send' command
+  ];
+
+  # Automatically installs, configures, and starts SwayNC
+  services.swaync.enable = true;
+
 }
