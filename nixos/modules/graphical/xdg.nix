2f4e1c32fd647812671783dde415330db2ab643b
Sreehari Anil <sreehari7102008@gmail.com>
2026-07-28 17:41:01 +0530

    dev: add kde portal

--- nixos/modules/graphical/xdg.nix
+++ nixos/modules/graphical/xdg.nix
@@ -10,12 +10,25 @@
     extraPortals = with pkgs; [
       xdg-desktop-portal-hyprland
       xdg-desktop-portal-gtk
+      kdePackages.xdg-desktop-portal-kde
     ];
-
     config = {
       common = {
         default = [
           "hyprland"
+          "kde"
+          "gtk"
+        ];
+      };
+      hyprland = {
+        default = [
+          "hyprland"
+          "gtk"
+        ];
+      };
+      plasma = {
+        default = [
+          "kde"
           "gtk"
         ];
       };
