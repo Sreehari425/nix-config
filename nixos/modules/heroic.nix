9b8e6b67a6c0bfee171ded67211097cf66b5383d
Sreehari Anil <sreehari7102008@gmail.com>
2026-08-12 11:35:18 +0530

    dev: added mangohud

--- nixos/modules/heroic.nix
+++ nixos/modules/heroic.nix
@@ -1,16 +1,18 @@
 { pkgs, ... }:
 
 {
+  programs.gamemode.enable = true;
+  programs.gamescope.enable = true;
+
   environment.systemPackages = [
+    pkgs.mangohud
     (pkgs.heroic.override {
       extraPkgs =
         pkgs': with pkgs'; [
           gamescope
           gamemode
+          mangohud
         ];
     })
   ];
-
-  programs.gamescope.enable = true;
-  programs.gamemode.enable = true;
 }
