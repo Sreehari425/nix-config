6848d7634506789aa5b4a9e417884e72c875527b
Sreehari Anil <sreehari7102008@gmail.com>
2026-09-10 00:44:28 +0530

    dev: disable fctix

--- home/configs/hypr/hyprland.lua
+++ home/configs/hypr/hyprland.lua
@@ -27,7 +27,7 @@ hl.on("hyprland.start", function()
     hl.exec_cmd("gnome-keyring-daemon --start --components=secrets")
     hl.exec_cmd("~/.config/hypr/scripts/battery_notify.sh")
 
-    hl.exec_cmd("fcitx5 -d")
+    -- hl.exec_cmd("fcitx5 -d")
     -- hl.exec_cmd("swaync")
     hl.exec_cmd("systemctl --user start hyprpolkitagent")
     -- hl.exec_cmd("hypridle")
